1c575cacc17e3f174c4ca1f2108a0a00faeaf552
XLGLJX <1084737354@qq.com>
2022-01-08 22:23:23 +0800

    更改

---
+++ CodeForces/hello 2022/A.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstring>
+#include <cstdio>
+#include <vector>
+#include <ctime>
+#include <queue>
+#include <bitset>
+#include <set>
+#include <cmath>
+#include <deque>
+#include <algorithm>
+#include <cctype>
+#define I inline
+#define R register
+#define LL long long
+#define INF (0x3f3f3f3f)
+#define lowbit(x) (x&(-x))
+using namespace std;
+I int read()
+{
+	int x=0;
+	char ch=0;
+	bool w=true;
+	while(!isdigit(ch)){if(ch=='-')w=false;ch=getchar();}
+	while(isdigit(ch)){x=(x<<3)+(x<<1)+(ch^48);ch=getchar();}
+	return w?x:-x;
+}
+I int MAIN()
+{
+	return 0;
+}
+int lglj=MAIN();
+int main(){;}
